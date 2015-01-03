730d7742a401e250f899ad8dbcb4f2c3cf73112f
樊俊 <powerfj@gmail.com>
2015-01-03 19:12:41 +0800

    增加运行权限，确保tmp目录

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+mkdir ~/tmp/
 echo "copy ./fcp to /usr/local/bin"
 cp ./fcp /usr/local/bin/
+chmod u+x /usr/local/bin/fcp
 echo "copy ./txt2qr to /usr/local/bin"
 cp ./txt2qr /usr/local/bin/
+chmod u+x /usr/local/bin/txt2qr
